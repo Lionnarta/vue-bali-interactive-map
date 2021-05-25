58eb6d8713b238652bf163aec501b815db4507bf
lionnarta <lionnarta.savirandy2000@gmail.com>
2021-05-25 22:56:55 +0700

    final for now

--- src/components/Map/MapContent.vue
+++ src/components/Map/MapContent.vue
@@ -24,20 +24,12 @@ export default {
           topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {id: "MapId",tileSize: this.tileSize, zoomOffset: this.zoomOffset ,attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'}),
           stadia = L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {id: "MapId",tileSize: this.tileSize, zoomOffset: this.zoomOffset ,attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'}),
           wi = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {id: "MapId",tileSize: this.tileSize, zoomOffset: this.zoomOffset ,attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
-      
+
       var southWest = L.latLng(-8.874141, 113.960504),
           northEast = L.latLng(-8.017000, 116.260766),
           bounds = L.latLngBounds(southWest, northEast);      
 
-      var map = L.map("map-container", {
-        center: this.center,
-        zoom: this.zoom,
-        minZoom: 10,
-        maxBounds: bounds,
-        layers: [stadia]
-      });
-
-      L.geoJSON(MapContentData, {
+      var polyLineMarker = L.geoJSON(MapContentData, {
         onEachFeature: function(feature, layer) {
           if (layer instanceof L.Polygon) {
             layer.setStyle({
@@ -49,7 +41,17 @@ export default {
             })
           }
         }
-      }).addTo(map);
+      });
+
+      var polyLineLayer = L.layerGroup([polyLineMarker]);
+
+      var mapContent = L.map("map-container", {
+        center: this.center,
+        zoom: this.zoom,
+        minZoom: 10,
+        maxBounds: bounds,
+        layers: [wi]
+      });
 
       var baseMap = {
         "Street" : street,
@@ -58,8 +60,12 @@ export default {
         "World Imagery": wi
       };
 
-      L.control.layers(baseMap, null, {position: "bottomright"}).addTo(map);
-      L.control.scale({position: "topright"}).addTo(map);
+      var overlayMap = {
+        "Map Lines": polyLineLayer
+      }
+
+      L.control.layers(baseMap, overlayMap, {position: "bottomright"}).addTo(mapContent);
+      L.control.scale({position: "topright"}).addTo(mapContent);
     },
     // async getJsonFromApi() {
     //   const res = await fetch(api_url);
---
+++ src/components/Map/MapMarkerData.js
@@ -0,0 +1,133 @@
+export const MapMarkerData = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Denpasar"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.672671,
+            115.213222
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Gianyar"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.539622,
+            115.330011
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Badung"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.578076,
+            115.172989
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Klungkung"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.542077,
+            115.403078
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Karangasem"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.448106,
+            115.607542
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Bangli"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.454855,
+            115.354940
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Buleleng"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.227810,
+            114.925379
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "marker-color": "#007bff",
+          "marker-size": "medium",
+          "marker-symbol": "",
+          "marker-name": "Jembrana"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -8.367505,
+            114.625169
+          ]
+        }
+      },
+    ]
+  }
